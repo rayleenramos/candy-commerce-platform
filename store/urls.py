7c93ef5c3955806cc0cdbdd048ba13a982cd64e5
Cesar Marte <cesar@Cesars-MacBook-Pro.local>
2025-12-03 18:29:27 -0500

    Implemented staff inventory management

--- store/urls.py
+++ store/urls.py
@@ -8,4 +8,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("candy/<int:candy_id>/", views.candy_detail, name="candy_detail"),
+    path("inventory/", views.inventory_list, name="inventory_list"),
+    path("inventory/update/<int:candy_id>/", views.update_stock, name="update_stock"),
 ]
